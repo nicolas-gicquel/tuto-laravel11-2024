7e2acf731a58c69b32bbf573be383f825ad39285
myrdinaptaliesen <nicog.dev@gmail.com>
2024-04-24 16:04:35 +0200

    Authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'pseudo' => ['required', 'string', 'max:40'],
+            'image' => ['required', 'string', 'max:40'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -63,8 +64,10 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // dd($data);
         return User::create([
-            'name' => $data['name'],
+            'pseudo' => $data['pseudo'],
+            'image' => $data['image'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
